f73b8888279ef92083966d881d115c47b71d28e5
Daevah Taylaran <daevtayl@gmail.com>
2022-07-12 10:47:17 -0700

    added more messages

--- practice/remember.js
+++ practice/remember.js
@@ -6,11 +6,16 @@ let messages = [
   "Never give up on yourself and building the Life that you are proud of.",
   "Accept your circumstance and open yourself up to gratitude and love.",
   "Be strong and True.",
+  "Commit and stay present. Love where you are at. Have gratitude right now.",
+  "Breathe. Enjoy your breath. Enjoy the Light that is right now.",
+  "See reality as it is right now for you.",
+  "Do not identify with circumstances that are irrelevant to how your Life is at in the present, to where your focus should be on.",
+  "See Life with joy and compassion. Continue to learn and grow mindfully.",
 ];
 
 function giveRandomMsg(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
-}
+};
 
 function giveMessage() {
   let msgOne = giveRandomMsg(messages);
@@ -20,6 +25,6 @@ function giveMessage() {
   console.log(`First Message: ${msgOne}`);
   console.log(`Second Message: ${msgTwo === msgOne ? msgTwo = giveRandomMsg(messages) : msgTwo}`);
   console.log(`Third Message: ${msgTwo === msgThree || msgOne === msgThree ? msgThree = giveRandomMsg(messages) : msgThree}`);
-}
+};
 
 giveMessage();
