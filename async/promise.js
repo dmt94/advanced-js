5392bdc7f5a54232b177701b5ec7d19c9b7e2800
Daevah Taylaran <daevtayl@gmail.com>
2022-07-14 20:39:09 -0700

    more promise

--- async/promise.js
+++ async/promise.js
@@ -1,9 +1,24 @@
 const promise = new Promise((resolve, reject) => {
-  true ? resolve('it worked yay') : reject('err error found');
+  // true ? resolve('it worked yay') : reject('err error found');
+  if (true) {
+    resolve('Stuff worked');
+  } else {
+    reject('Error, it broke');
+  }
 });
 
 promise
-  .then(result => console.log(result + '!'))
-  .then(result2 => console.log(result2))
+  .then(result => {
+    // throw Error
+    return result + '!';
+  })
+  .then(result2 => {
+    return result2 + '?';
+    // throw Error
+  })
+  .catch(() => console.log('errorrr found'))
+  .then(result3 => {
+    console.log(result3 + '!');
+  })
 
 console.log(promise);
