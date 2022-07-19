2dadf09c28626058c5400c1c4acb43e1a18f0d20
Daevah Taylaran <daevtayl@gmail.com>
2022-07-18 20:34:45 -0700

    more async

--- async/promise.js
+++ async/promise.js
@@ -22,3 +22,30 @@ promise
   })
 
 console.log(promise);
+
+/*
+Promise can be assigned to a variable 
+Promises are good for async programming
+When you dont want JS to "block" execution of code,
+- optimize image, etc.
+you use a PROMISE so that task runs in the background
+
+when resolved => response
+
+*/
+
+const promise2 = new Promise((res, reject) => {
+  setTimeout(res, 100, 'HIII')
+})
+const promise3 = new Promise((res, reject) => {
+  setTimeout(res, 1000, 'HUNKLE')
+})
+const promise4 = new Promise((res, reject) => {
+  setTimeout(res, 3000, 'Is it me you are looking for?')
+})
+
+//logs all promises inside the array
+Promise.all([promise, promise2, promise3, promise4])
+  .then(values => {
+    console.log(values);
+  })
