0978d218e6e3457e4681d3b34050dfedf7bcd2ab
Daevah Taylaran <daevtayl@gmail.com>
2022-07-20 16:35:11 -0700

    more promises

--- async/more-promises.js
+++ async/more-promises.js
@@ -4,6 +4,20 @@ const urls = [
   'https://jsonplaceholder.typicode.com/albums',
 ];
 
+
+const getData = async function() {
+  try {
+    const [users, posts, albums] =  await Promise.all(urls.map(url =>
+      fetch(url).then(resp => resp.json())));
+    console.log('users', users);
+    console.log('posts', posts);
+    console.log('albums', albums);
+  } catch (error) {
+    console.log('oops. an error occured');
+  }
+}
+
+
 Promise.all(urls.map(url => {
   return fetch(url).then(resp => resp.json())
 })).then(results => {
