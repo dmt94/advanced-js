05f2ca91442b90caaf19ff057e97c0c357cb8c81
Daevah Taylaran <daevtayl@gmail.com>
2022-07-25 17:22:33 -0700

    data

---
+++ data-structures/data.js
@@ -0,0 +1,54 @@
+/*
+Data Structures + Algorithms = Programs
+
+Always learn fundamentals! 
+
+Data Structures 
+- collection of values
+- each values are different in what it can do and what it is used for
+- each data structure is good and specialized for its own thing
+
+- each container is useful for its own thing
+
+Data structures
+1. how to build one
+2. how to use it
+  > data structures are tools and most of the time,
+  they are prebuilt for us -- how to use one over the other?
+
+Goal:
+- understand data structures
+
+How Computers Store Data 
+- CPU
+  > computer storage 
+  > needs access to RAM and storage, 
+  but can access to RAM a lot faster
+  
+
+- RAM (random access memory)
+  >  form of computer memory that can be read and changed in any order, 
+  typically used to store working data and machine code.
+  > massive storage area (like data structure)
+  > have shelves that are numbered (addresses)
+    - each shelf holds 8 bits 
+    - bit = tiny electrical switch (byte = one) 1 or 0
+    - CPU asks RAM what is in shelf xyz?
+      - memory controller has connections to all of the shelves
+      - random access memory = access fast, access bits random fast
+      - closer info is to CPU = less time
+      - shelves closer to each other = less time
+      - has a small storage that stores recent memory = cache
+    
+- Storage
+  > persistent storage is slow
+  > saving application to computer => saves to storage
+
+Data Structure 
+- way for us to store information
+- var a = 1
+- we represent integers in 32 bits (or 64 bit)
+*/
+
+Math.pow(5, 100); //base, exponent
+console.log(Math.pow(6, 1000)); //base, exponent (Infinity)
